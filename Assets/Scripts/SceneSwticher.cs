04d202263fefe8882ae560a54d2c658019648353
JuyeonO <yexnx01@gmail.com>
2024-12-01 23:15:33 +0900

    feat: 씬 테스트

---
+++ Assets/Scripts/SceneSwticher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwticher : MonoBehaviour {
+
+	public GameObject FadeOut;
+	public int sceneIndex;
+
+	void Start ()
+    {
+		FadeOut.SetActive (false);
+	}
+	
+	void OnTriggerEnter (Collider other)
+    {
+
+		if (other.gameObject.tag == "Player")
+        {
+			StartCoroutine (SceneSwitch ());
+
+		}
+	}
+
+ //   public void SceneMobileExit()
+	//{
+	//	Application.Quit();
+	//}
+
+    public void SceneSwitchButton()
+    {
+        StartCoroutine(SceneSwitch());
+    }
+
+    IEnumerator SceneSwitch ()
+    {
+		FadeOut.SetActive (true);
+		yield return new WaitForSeconds (3);
+		SceneManager.LoadSceneAsync (sceneIndex);
+	}
+
+	
+}
